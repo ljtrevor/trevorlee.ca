337beb0e9c1f8b4532f9946961d5150749f7b6b2
ljtrevor <ljtrevor0689@gmail.com>
2018-09-29 01:56:49 -0700

    add scroll animation and nav bar active states

--- trevor_website/src/js/index.js
+++ trevor_website/src/js/index.js
@@ -1,53 +1,11 @@
+// Paddings used to space the SVG elements and make them look cleaner
+var padding_left=7;
+var padding_top=5;
+var padding_point=30;
+
 /*
 Get the position of an element relative to the document
 */
-
-
-
-
-// var c = canvas.getContext("2d");
-// c.beginPath();
-// c.moveTo(100,100);
-// c.lineTo(110,110);
-// c.lineWidth=.5;
-// c.stroke();
-
-
-
-// var point_start = document.getElementById("point_start");
-// var point_end = document.getElementById("point_end");
-// console.log(point_start);
-// console.log(point_end);
-// var pos_start = getElementPosition(point_start);
-// var pos_end = getElementPosition(point_end);
-// console.log(pos_start.left, pos_start.top);
-// console.log(pos_end.left, pos_end.top);
-
-
-// var canvas = $("#canvas");
-// var parent = $("#canvas_parent");
-// const width = parent.clientWidth;
-// const height = parent.clientHeight;
-
-// console.log(canvas);
-// console.log(parent);
-
-// canvas.width = width;
-// canvas.height = height;
-
-
-// // $canvas.width($parent.width());
-// // $canvas.height($parent.height());
-
-// // window.addEventListener('resize', resizeCanvas, false);
-// // resizeCanvas();
-
-// var canvas = document.querySelector('canvas');
-// var c = canvas.getContext('2d');
-// c.beginPath();
-// c.arc(100,100,10,0,Math.PI*2,false);
-// c.stroke();
-
 function getElementPosition(el) {
     var rect = el.getBoundingClientRect(),
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
@@ -55,71 +13,78 @@ function getElementPosition(el) {
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
 
-//createLine(503.3125, 1118.78125,503.3125,2176.78125,"line");
+/*
+Resize the canvas and the SVG line when the window width and height change.
+Also, redraws the line from the new positions.
+*/
+function resizeCanvas() {
+    pos1 = div1.offset();
+    pos2 = div2.offset();
+    svg.css('height', pos2.top + padding_point);
 
-// var line = $('#line');
-// var div1 = $('#one');
-// var div2 = $('#two');
+    line1
+  .attr('x1', pos1.left + padding_left)
+  .attr('y1', pos1.top + padding_top)
+  .attr('x2', pos2.left + padding_left)
+  .attr('y2', pos2.top + padding_left);
+}
 
-//console.log(one);
-
-// var x1 = div1.offset().left + (div1.width()/2);
-// var y1 = div1.offset().top + (div1.height()/2);
-// var x2 = div2.offset().left + (div2.width()/2);
-// var y2 = div2.offset().top + (div2.height()/2);
-
-// line.attr('x1',x1).attr('y1',y1).attr('x2',x2).attr('y2',y2);
-
-
-// var canvas;
-// var canvasWidth;
-// var ctx;
- 
-// canvas = document.getElementById('canvas');
-// window.addEventListener('resize', resizeCanvas, false);
-// window.addEventListener('orientationchange', resizeCanvas, false);
-// resizeCanvas();
- 
-// function resizeCanvas() {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-// }
-
-
+// Get the SVG line, the start point, and end point of the line.
 line1 = $('#line1');   
 div1 = $('#point_start');   
 div2 = $('#point_end');
 
+//Get the document position of the start point and end point for the line.
 var pos1 = div1.offset();
 var pos2 = div2.offset();
-console.log(pos1);
-console.log(pos2);
 
+// Set the SVG height dynamically (the width is set to 100% in css)
+var svg = $('#svg1');
+svg.css('height', pos2.top + padding_point);
 
+// Draw the line from the initial start and end points.
 line1
-  .attr('x1', pos1.left+7)
-  .attr('y1', pos1.top+5)
-  .attr('x2', pos2.left+7)
-  .attr('y2', pos2.top+5);
+  .attr('x1', pos1.left + padding_left)
+  .attr('y1', pos1.top + padding_top)
+  .attr('x2', pos2.left + padding_left)
+  .attr('y2', pos2.top + padding_top);
 
-
-  var svg = $('#svg1');
-  svg.css('height', pos2.top+30);
-
+// Event listener to resize the svg canvas and redraw the line when the window is resized.
   window.addEventListener('resize', resizeCanvas, false);
   window.addEventListener('orientationchange', resizeCanvas, false);
   resizeCanvas();
 
-function resizeCanvas() {
-    var pos1 = div1.offset();
-    var pos2 = div2.offset();
-    console.log(pos1);
-    console.log(pos2);
-    
-    line1
-  .attr('x1', pos1.left+7)
-  .attr('y1', pos1.top+5)
-  .attr('x2', pos2.left+7)
-  .attr('y2', pos2.top+5);
-}
 
+  // Navbar smooth scroll effect
+  var scrollLink = $('.scroll');
+  scrollLink.click(function(e){
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000);
+  })
+
+  //Active link switching
+
+  //if on top of the page, home should be selected
+  if($(window).scrollTop() === 0) {
+    $('#nav-home').addClass('active');
+    $('#nav-home').parent().siblings().children().removeClass('active');
+
+  }
+  $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      scrollLink.each(function() {
+          var sectionOffset = $(this.hash).offset().top - 10;
+          if(sectionOffset <= scrollbarLocation) {
+              $(this).addClass('active');
+              $(this).parent().siblings().children().removeClass('active');
+          }
+
+          //case for contact since the navbar never touches the contact section (trigger with bootom of page hit)
+          if($(window).scrollTop() + $(window).height() == $(document).height()) {
+            $('#nav-contact').addClass('active');
+            $('#nav-contact').parent().siblings().children().removeClass('active');
+        }
+      })
+  })
